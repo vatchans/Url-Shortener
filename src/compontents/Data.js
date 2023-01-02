5731e52f8f719243d9764fc620d750cb6963ac0c
vatchans <112924038+vatchans@users.noreply.github.com>
2023-01-02 21:20:31 +0530

    Update Data.js

--- src/compontents/Data.js
+++ src/compontents/Data.js
@@ -43,7 +43,7 @@ function Data() {
         </div>
        
         <div  className="clip-icon" style={{ display: 'flex', justifyContent: 'center' }}>
-        <img src="scissors (1).png"width="40px"></img>
+        <img src="scissors (1).png"width="40px" alt="...'></img>
         </div>
         <div class="vl" style={{ display: 'flex', justifyContent: 'center' }}></div>
         <Outlet/>
