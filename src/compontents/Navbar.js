0d2148a59bed72c72b3bd0ccae6844fab948802d
vatchans <112924038+vatchans@users.noreply.github.com>
2023-01-03 18:28:50 +0530

    Update Navbar.js

--- src/compontents/Navbar.js
+++ src/compontents/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
     <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+    <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div className="offcanvas-header" >
         <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Url-Shortener</h5>
         <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -23,20 +23,21 @@ function Navbar() {
             <a className="nav-link dropdown-toggle" href="javascript void" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               My links
             </a>
-            <ul className="dropdown-menu">
-              {data.map((e)=>{
+              {data.map((e,i)=>{
                   const url=`https://url-u7rz.onrender.com/${e.Shorturl}`
                   const Fullurl=`https://${e.Fullurl.slice(8)}/favicon.ico`
               return<>
+              <ul className="dropdown-menu"key={i}>
              <li>
                 <hr className="dropdown-divider"/>
               </li>
-              <li><img src={Fullurl} width="20px" alt="url-favicon"></img>&nbsp;<a href={url} style={{fontSize:"14px"}}target='_blank' rel="noreferrer">{url}</a>
+              <li ><img src={Fullurl} width="20px" alt="url-favicon"></img>&nbsp;<a href={url} style={{fontSize:"14px"}}target='_blank' rel="noreferrer">{url}</a>
               </li>
+              </ul>
               </>
             })
-            }
-            </ul>
+          }
+          
           </li>
         </ul>
       </div>
